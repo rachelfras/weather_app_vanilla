3f49cd5e269934c45f36a4cb592f251fb0d56b6c
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-07-08 15:26:32 +0100

    Added JS for current button
    Will now display current city and temperature.
    Still to add - min/max temp, location relevant time, precipitation and wind.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+let apiKey = `fa1047ba99073894a88e54f4a5673a70`;
+
 function callDate(){
     let displayDay = document.querySelector("#weekday");
     displayDay.innerHTML = currentDay;
@@ -15,5 +17,33 @@ let minutes = `0${rightNow.getMinutes()}`.slice(-2);
 let currentTime = `${hour}:${minutes}`;
 
 callDate();
+
+function displayCurrent(response){
+    let temp = Math.round(response.data.main.temp);
+    let bigTemp = document.querySelector("#big-temp");
+    bigTemp.innerHTML = temp;
+
+    let city = document.querySelector(".location-display");
+    city.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+}
+
+function giveCurrent(position){
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
+    
+    axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCurrent);
+}
+
+function search(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(giveCurrent);
+}
+
+let form = document.querySelector("#search-bar");
+form.addEventListener("click", search);
+
+
 
  
