619dde30871255780765c1706ee544d7f9548f00
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-07-08 22:52:57 +0100

    Added default city search on load

--- src/script.js
+++ src/script.js
@@ -21,23 +21,22 @@ callDate();
 function displayCityTemp(response){
     let temp = Math.round(response.data.main.temp);
     let bigTemp = document.querySelector("#big-temp");
-    bigTemp.innerHTML = temp;
-
     let city = document.querySelector(".location-display");
+    
+
+    bigTemp.innerHTML = temp;
     city.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+    
 }
 
-function search(event) {
-    event.preventDefault();
-    let inputCity = document.querySelector("#enter-city");
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}`;
+function search(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}`;
     let units = `metric`;
 
     axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCityTemp);
-    inputCity.value = "";
 }
 
-function giveCurrent(position){
+function giveCurrent(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let units = "metric";
@@ -46,17 +45,26 @@ function giveCurrent(position){
     axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCityTemp);
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    let inputCity = document.querySelector("#enter-city");
+    search(inputCity.value);
+    inputCity.value = "";
+}
+
 function searchGeolocation(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(giveCurrent);
 }
 
 let form = document.querySelector("#search-bar");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
 
 
 let currentCity = document.querySelector(".current");
 currentCity.addEventListener("click", searchGeolocation);
+
+search("Tokyo");
 
 
 
