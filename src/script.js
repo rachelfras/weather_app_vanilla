42787f2b1a21fc16bdfdf3fc6b897bc24615078f
rachelfras <82391537+rachelfras@users.noreply.github.com>
2021-07-08 16:54:12 +0100

    Added search function JS
    Added pathway for search function.
    Fixed bug from draft - search button is now an input element so allows both "submit" and "click" events.

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ let currentTime = `${hour}:${minutes}`;
 
 callDate();
 
-function displayCurrent(response){
+function displayCityTemp(response){
     let temp = Math.round(response.data.main.temp);
     let bigTemp = document.querySelector("#big-temp");
     bigTemp.innerHTML = temp;
@@ -27,22 +27,36 @@ function displayCurrent(response){
     city.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
 }
 
+function search(event) {
+    event.preventDefault();
+    let inputCity = document.querySelector("#enter-city");
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}`;
+    let units = `metric`;
+
+    axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCityTemp);
+    inputCity.value = "";
+}
+
 function giveCurrent(position){
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let units = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
     
-    axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCurrent);
+    axios.get(`${apiUrl}&units=${units}&appid=${apiKey}`).then(displayCityTemp);
 }
 
-function search(event) {
+function searchGeolocation(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(giveCurrent);
 }
 
 let form = document.querySelector("#search-bar");
-form.addEventListener("click", search);
+form.addEventListener("submit", search);
+
+
+let currentCity = document.querySelector(".current");
+currentCity.addEventListener("click", searchGeolocation);
 
 
 
